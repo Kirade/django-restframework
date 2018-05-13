193a32074a8f449b642b9a3ca798f43ef1e68a41
허재영 <jay.jaeyoung@gmail.com>
2018-05-13 22:30:11 +0900

    Add viewset

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class userViewSet(viewsets.ModelViewSet):
+    queryset = user.objecst.all().order_by('date_joined')
+    serializer_class = UserSerializer
+
+
+class userViewSet(viewsets.ModelViewSet):
+    queryset = Group.objecst.all()
+    serializer_class = GroupSerializer
+
